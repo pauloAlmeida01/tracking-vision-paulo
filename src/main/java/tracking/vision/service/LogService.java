5405a6977f6da219ce078005e2d9f603b6922dae
pauloAlmeida01 <paulo.rsantos@sptech.school>
2023-05-06 11:54:36 -0300

    jar

--- src/main/java/tracking/vision/service/LogService.java
+++ src/main/java/tracking/vision/service/LogService.java
@@ -11,7 +11,7 @@ public class LogService {
 
         JdbcTemplate con = conexao.getConnection();
         JdbcTemplate conMysql = conexaoMysql.getConnection();
-
+        //conexao
         con.update("insert into log(horarioCapturado,janelaPid,tituloJanela,usoCpu,usoDisco,usoRam,bytesRecebidos,bytesEnviados,fkMaquina) values (?, ?, ?, ?, ?,?,?,?,?)", log.getHorarioCapturado(), log.getJanelaPid(), log.getTituloJanela(), log.getUsoCpu(), log.getUsoDisco(), log.getUsoRam(),log.getBytesRecebidos(),log.getBytesEnviados(), log.getFkMaquina());
         conMysql.update("insert into log(horarioCapturado,janelaPid,tituloJanela,usoCpu,usoDisco,usoRam,bytesRecebidos,bytesEnviados,fkMaquina) values (?, ?, ?, ?, ?,?,?,?,?)", log.getHorarioCapturado(), log.getJanelaPid(), log.getTituloJanela(), log.getUsoCpu(), log.getUsoDisco(), log.getUsoRam(),log.getBytesRecebidos(),log.getBytesEnviados(), log.getFkMaquina());
     }
