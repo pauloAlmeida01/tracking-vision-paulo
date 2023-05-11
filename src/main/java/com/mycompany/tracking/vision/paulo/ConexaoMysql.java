e54161c19bdc0194b261b02ea3663cf8638bf75d
pauloAlmeida01 <paulo.rsantos@sptech.school>
2023-05-11 17:30:22 -0300

    build jar

--- src/main/java/com/mycompany/tracking/vision/paulo/ConexaoMysql.java
+++ src/main/java/com/mycompany/tracking/vision/paulo/ConexaoMysql.java
@@ -19,7 +19,7 @@ public class ConexaoMysql {
 
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
-        dataSource.setUrl("jdbc:mysql://localhost:3306/track?autoReconnect=true&useSSL=false");
+        dataSource.setUrl("jdbc:mysql://172.17.0.2:3306/track?autoReconnect=true&useSSL=false");
 
         dataSource.setUsername("root");
 
