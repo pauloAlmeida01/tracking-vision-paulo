9a0324813684423b734278406b83f5539d43f914
pauloAlmeida01 <paulo.rsantos@sptech.school>
2023-06-02 18:31:30 -0300

    Build jar

--- src/main/java/tracking/vision/service/MaquinaService.java
+++ src/main/java/tracking/vision/service/MaquinaService.java
@@ -22,7 +22,7 @@ public class MaquinaService {
         JdbcTemplate con = conexao.getConnection();
         JdbcTemplate conMysql = conexaoMysql.getConnection();
 
-        con.update("insert into maquina(status,hostnameMaquina,nomeModeloCpu,clockCpu,nomeModeloRam,capacidadeTotalRam,nomeModeloDisco,capacidadeTotalDisco,leituraDisco,escritaDisco,fkEmpresa) values (?,?,?,?,?,?,?,?,?,?,?)", 1,maquina.getHostnameMaquina(), maquina.getNomeModeloCpu(), maquina.getClockCpu(), maquina.getNomeModeloRam(), maquina.getCapacidadeTotalRam(), maquina.getNomeModeloDisco(), maquina.getCapacidadeTotalDisco(), maquina.getLeituraDisco(), maquina.getEscritaDisco(),maquina.getFkEmpresa());
+        con.update("insert into maquina(statusMaquina,hostnameMaquina,nomeModeloCpu,clockCpu,nomeModeloRam,capacidadeTotalRam,nomeModeloDisco,capacidadeTotalDisco,leituraDisco,escritaDisco,fkEmpresa) values (?,?,?,?,?,?,?,?,?,?,?)", 1,maquina.getHostnameMaquina(), maquina.getNomeModeloCpu(), maquina.getClockCpu(), maquina.getNomeModeloRam(), maquina.getCapacidadeTotalRam(), maquina.getNomeModeloDisco(), maquina.getCapacidadeTotalDisco(), maquina.getLeituraDisco(), maquina.getEscritaDisco(),maquina.getFkEmpresa());
     }
 
     public void salvarMaquinaMysql(Maquina maquina) {
